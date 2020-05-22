4ed8058bb0de53e0fcd1b326678f72add681804d
bear49193 <rbeer199525@gmail.comgit>
2020-05-22 07:10:20 +0530

    Add build

---
+++ client/build/precache-manifest.291ed7eb657bb616da2c5d15732c5a2c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b14a6b6862ed2c054a7056ece11c31eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34c65205d10287729250",
+    "url": "/static/css/2.e61fc129.chunk.css"
+  },
+  {
+    "revision": "8a6e74cb0f528fcaf313",
+    "url": "/static/css/main.d64d46dd.chunk.css"
+  },
+  {
+    "revision": "34c65205d10287729250",
+    "url": "/static/js/2.54e16173.chunk.js"
+  },
+  {
+    "revision": "d8d68c3211389b522dc60ba759db8725",
+    "url": "/static/js/2.54e16173.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a6e74cb0f528fcaf313",
+    "url": "/static/js/main.17fb9c65.chunk.js"
+  },
+  {
+    "revision": "cda345fb50b7e11c0025",
+    "url": "/static/js/runtime-main.844668cf.js"
+  },
+  {
+    "revision": "de6485bc42be4d451611670d94ec98ea",
+    "url": "/static/media/Polygon.de6485bc.svg"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "9165801ceddc7445e4b437a1d836a513",
+    "url": "/static/media/field.9165801c.jpg"
+  },
+  {
+    "revision": "b27b7f1b45b98eea63ae33768ad65545",
+    "url": "/static/media/forest.b27b7f1b.jpg"
+  },
+  {
+    "revision": "018d2ac351d2f2de9b77e870f29894e1",
+    "url": "/static/media/loading.018d2ac3.gif"
+  },
+  {
+    "revision": "a5e18f2acd5ae09d95b3b66885b17565",
+    "url": "/static/media/mount.a5e18f2a.jpg"
+  }
+]);

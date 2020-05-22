23532671a94cd6d4a4794240770c0f6cb4b07f23
bear49193 <rbeer199525@gmail.comgit>
2020-05-22 12:29:29 +0530

    Add heroku config

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.use('/api', routes);
 
 app.use(express.static("client/build"));
   
-app.get("/*", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 

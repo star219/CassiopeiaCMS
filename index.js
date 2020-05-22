3a071dc93fd13175ac6d20bd2b6772f3c7048478
bear49193 <rbeer199525@gmail.comgit>
2020-05-22 07:57:58 +0530

    Add path

--- index.js
+++ index.js
@@ -36,9 +36,12 @@ app.use(session({
   maxAge: 5 * 60 * 60 * 1000, // 8 hours
 }))
 app.use('/api', routes);
+
+//app.use(express.static(path.join(__dirname, 'build')));
+
 app.use(express.static("client/build"));
   
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 
